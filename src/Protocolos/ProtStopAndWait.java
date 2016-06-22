907f993e47f02db6ab43d521a14f7594df022d19
ian <ianmalm.ufrj@gmail.com>
2016-06-22 01:30:01 -0300

    Alterações

--- src/Protocolos/ProtStopAndWait.java
+++ src/Protocolos/ProtStopAndWait.java
@@ -2,9 +2,13 @@ package Protocolos;
 
 public class ProtStopAndWait {
 
+	private final static int TAMANHO_QUADRO = 4;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		
 	}
 
 }
