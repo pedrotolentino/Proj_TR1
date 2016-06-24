5a8def8cbb210bfe701b03f750371010a9eb60cb
Ian Schonmann Malm <ianmalm.ufrj@gmail.com>
2016-06-23 21:27:36 -0300

    Commit

--- src/Protocolos/ProtStopAndWait.java
+++ src/Protocolos/ProtStopAndWait.java
@@ -4,11 +4,16 @@ public class ProtStopAndWait {
 
 	private final static int TAMANHO_QUADRO = 4;
 	
+	public void emissor(){
+	
+	}
+	
+	public void receptor(){
+	
+	}
+	
 	public static void main(String[] args) {
-		
-		
-		
-		
+	
 	}
 
 }
