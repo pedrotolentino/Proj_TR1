7c8fa40ca59fac3992447110c3101afac6cff6dd
Ian Schonmann Malm <ianmalm.ufrj@gmail.com>
2016-06-28 18:32:10 -0300

    Consertando ProtSelectiveRepeat

--- src/Protocolos/ProtSelectiveRepeat.java
+++ src/Protocolos/ProtSelectiveRepeat.java
@@ -13,47 +13,39 @@ public class ProtSelectiveRepeat implements Protocolo{
 	public void enviarPacote(ObjectInputStream in, ObjectOutputStream out, Vector pacote) throws IOException, ClassNotFoundException {
 		int flagRetorno = -1;
 		int[] ret = null;
-		int[] reenvio = null;
-		int k = 0;
-		Vector novoPacote = new Vector();
+		Vector novoPacote = null;
 		while(flagRetorno != ACK){
 			flagRetorno = ACK;
+			int erro = 0;
 			out.reset();
 			out.writeObject(TRANSMISSAO);
-			if(ret == null){
-				out.reset();
-				out.writeObject(pacote);
-			}else{
-				k = 0;
-				for(int j = 0; j < pacote.size(); j++){
-					for(int l = 0; l < reenvio.length; l++){
-						if(reenvio[l] == j){
-							novoPacote.add(pacote.get(j));
-						}
-					}
-				}
-				pacote = novoPacote;
-				out.reset();
-				out.writeObject(pacote);
-			}
+			out.writeObject(pacote);
+
 			System.out.print("Emi -> ");
 			try{
 				ret	 = (int[]) in.readObject();
 				
 			}catch(InterruptedByTimeoutException e){}
-			
+						
 			System.out.println();
-			
 			for(int i = 0; i < ret.length; i++){
 				if(ret[i] == NACK){
-					reenvio = new int[ret.length];
-					System.out.println("NACK do pacote "+(i+1)+"");
 					flagRetorno = NACK;
-					reenvio[k] = i;
-					k++;
+					erro++;
+					System.out.println("NACK do pacote "+(i+1)+"");
+
 				}else if(ret[i] == ACK){
 					System.out.println("ACK do pacote "+(i+1));
 				}
+			}
+			if(flagRetorno == NACK){
+				novoPacote = new Vector<>(erro);
+				for(int i = 0; i < ret.length; i++){
+					if(ret[i] == NACK){
+						novoPacote.add(pacote.get(i));
+					}
+				}
+				pacote = novoPacote;
 			}
 		}
 	}
